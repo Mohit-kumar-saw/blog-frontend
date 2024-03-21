7b521e482f027cfc169d7d5ef0ae19c15a6e7f3e
Mohit-kumar-saw <mohitsaw19@gmail.com>
2024-03-21 19:44:22 +0530

    update

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -117,7 +117,7 @@ const Card = ({ prop }) => {
                 <Link to={`/details/${item._id}`} className="link">
                   <h3>{item.title}</h3>
                 </Link>
-                <p>{item.desc.slice(0, 180)}...</p>
+                <p className="desc">{item.desc.slice(0, 180)}...</p>
                 <div className="date">
                   <AiOutlineClockCircle className="icon" />
                   <label htmlFor="">{item.date}</label>
